0fd6d884441fafc7ded2a878b75826e600c9ed7e
Esma <esmazuurbier12@gmail.com>
2022-05-20 12:29:33 +0200

    added struct for coordinates and stack class

--- maze.cpp
+++ maze.cpp
@@ -3,12 +3,42 @@ Where A is the starting point and B the goal */
 
 #include <iostream>
 #include <vector>
+#include <deque>
 #include <utility>
 
 using namespace std;
 
+struct Coordinate {
+  int x, y;
+  Coordinate(int a, int b) { this->x = a; this->y = b; }
+};
+
 class Node {
-    pair<int, int> state;
+    Coordinate state;
     Node* parent;
-    pair<int, int> action;
+    Coordinate action;
+};
+
+class StackFrontier {
+    deque<Node> frontier;
+    StackFrontier() {frontier;}
+
+    void add(Node n) {
+        frontier.push_back(n);
+    }
+
+    Coordinate contains_state(Coordinate state) {
+        //TODO
+    }
+
+    Node remove() {
+        if (frontier.empty()) {
+            throw std::underflow_error("empty frontier");
+        }
+        else {
+            Node node = frontier.back();
+            frontier.pop_back();
+            return node;
+        }
+    }
 };
