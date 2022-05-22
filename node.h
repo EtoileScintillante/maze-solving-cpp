ab8ce9fd0e0baef76916aa76d01878ef64e4f0e8
Esma <esmazuurbier12@gmail.com>
2022-05-22 13:41:13 +0200

    made node.h

---
+++ node.h
@@ -0,0 +1,13 @@
+#include "coordinate.h"
+#include <stdio.h>
+
+class Node {
+public:
+    Node() {state = Coordinate(); parent = NULL;}
+    Coordinate state;
+    Node* parent;
+    Node(Coordinate State, Node* Parent) {
+        state = State;
+        parent = Parent;
+    }
+};
