5b82f706dbf637f7582d2fe84344b93896d5cad6
Esma <esmazuurbier12@gmail.com>
2022-05-26 21:05:27 +0200

    fixed free memory funct

--- headers/stackfrontier.h
+++ headers/stackfrontier.h
@@ -35,14 +35,12 @@ public:
             return node;
         }
     }
-    
+
     // Free allocated memory
     void deleteFrontier() {
 
         for (int i = 0; i < frontier.size(); i++) {
             delete frontier[i];
         }
-
-        frontier.clear();
     }
 };
