9163d995fd65b3425907757e3f99b6201def0853
Esma <esmazuurbier12@gmail.com>
2022-07-06 16:53:39 +0200

    edited comment

--- src/include/maze.h
+++ src/include/maze.h
@@ -47,7 +47,7 @@ public:
     // Prints the maze
     void print();
 private:
-    // Every node has a state and 4 possible neighbor states (up, down, left and right)
+    // Every node has a state which has 4 possible neighbor states (up, down, left and right)
     // This function returns a vector containing the neighbor states
     std::vector<Coordinate> neighbors(Coordinate state);
 public:
