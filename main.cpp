379c2ade5d0ed9440235ded3c5dbff070df36845
Esma <esmazuurbier12@gmail.com>
2022-07-07 10:21:58 +0200

    made it easier to switch between lifo and fifo

--- main.cpp
+++ main.cpp
@@ -7,16 +7,22 @@ using namespace std;
 int main(int argc, char* argv[]) {
 
     // Check if correct number of arguments is provided
-   if (argc != 2) {
-        cout << "Usage: ./maze-solver path-to-maze" << endl;
+    if (argc != 3) {
+        cout << "Usage: ./maze-solver path-to-maze bool" << endl;
     }
 
+    // Set LIFO to true or false depending in given argument
+    bool LIFO;
+    if (strcasecmp(argv[2], "true") == 0) {LIFO = true;}
+    if (strcasecmp(argv[2], "false") == 0) {LIFO = false;}
+
+    // Ananlyze and solve the maze
     Maze m;
     m.validate_file(argv[1]);
     m.print();
     cout << "Solving..." << endl;
     cout << endl;
-    m.solve();
+    m.solve(LIFO);
     m.print();
     cout << "States explored: " << m.num_explored << endl;
 
